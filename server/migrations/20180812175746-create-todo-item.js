20c906c5822f88108434f7750d400ba6f1c18921
andela-fomokaro <faith.omokaro@andela.com>
2018-08-12 20:27:10 +0100

    add method for posting todo

---
+++ server/migrations/20180812175746-create-todo-item.js
@@ -0,0 +1,31 @@
+/* eslint-disable */
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('TodoItems', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      complete: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('TodoItems');
+  }
+};
